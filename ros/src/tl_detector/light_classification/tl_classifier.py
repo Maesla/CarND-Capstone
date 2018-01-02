aeea937563a6c44dc929ca88cf8fe422daacadd0
vc77 <veeken77@gmail.com>
2018-01-01 20:44:51 -0500

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -29,7 +29,7 @@ class TLClassifier(object):
         self.tf_session = None
         self.predict = None
         self.clabels = [4, 2, 0, 1, 4, 4]
-        self.readsize = 1024
+        self.model_path = model_path
 
     def get_classification(self, image_np):
         """Determines the color of the traffic light in the image
@@ -45,7 +45,10 @@ class TLClassifier(object):
         # we thank Anthony S. for the annotations to the bag data made available through link in slack channel for this project
         # using these feature set sepcs:
         # https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md
-
+        
+	ros_root = rospkg.get_ros_root()
+        r = rospkg.RosPack()
+        path = r.get_path('tl_detector')
         
         # set up tensorflow and traffic light classifier
         if self.tf_session is None:
@@ -53,7 +56,7 @@ class TLClassifier(object):
             self.config = tf.ConfigProto(log_device_placement=True)
             self.config.gpu_options.per_process_gpu_memory_fraction = 0.5  # don't hog all the VRAM!
             self.config.operation_timeout_in_ms = 50000 # terminate anything that don't return in 50 seconds
-            self.tf_graph = load_graph(self.model_path + '.pb')
+            self.tf_graph = load_graph(path + self.model_path + '.pb')
 
 
             with self.tf_graph.as_default():
