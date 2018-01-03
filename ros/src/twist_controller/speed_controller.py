e8aa4efe4ab0688da9e0c0e3c5fd7a81757c2ac1
Maesla <espinolaramanuel@gmail.com>
2018-01-03 09:35:00 -0800

    Noisy throttle fixed. It was a PID problem

--- ros/src/twist_controller/speed_controller.py
+++ ros/src/twist_controller/speed_controller.py
@@ -3,10 +3,8 @@ from pid import PID
 
 class Speed_Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        
-        #self.pid = PID(0.0476519, -8.561e-05, 0.201947)
-        self.pid = PID(0.2, 0.005, 0.1)
+
+        self.throttle_pid = PID(0.2, 0.005, 0.1)
         
         self.mass = None
         self.fuel_capacity = None
@@ -16,9 +14,12 @@ class Speed_Controller(object):
         self.max_deceleration = None
         self.max_brake_torque = None
         self.rate = None
+        self.dt = None
+
 	
     def setup(self, rate, m, fuel_capacity, density, wheel_radius, max_deceleration):
         self.rate = rate
+        self.dt = 1.0/rate
         self.mass = m
         self.fuel_capacity = fuel_capacity
         self.fuel_density = density
@@ -34,20 +35,17 @@ class Speed_Controller(object):
         
     def control(self, current_speed, target_speed):
         speed_error = target_speed - current_speed
-
-        dt = 1.0/self.rate
         
         throttle = 0
         brake = 0
         if (speed_error >= 0):
-            throttle = self.pid.step(speed_error, dt)
+            throttle = self.throttle_pid.step(speed_error, self.dt)
             throttle = self.clamp(throttle, 0.0,1.0)
         else:
-			acc = speed_error/dt
+			acc = speed_error/self.dt
 			brake = self.calculate_brake(self.total_mass, acc, self.wheel_radius)
 			brake = self.clamp(brake, 0, self.max_brake_torque)
-        
-        
+			self.throttle_pid.reset()
         
         return throttle, brake
         
@@ -59,4 +57,4 @@ class Speed_Controller(object):
 		return value
 		
     def reset(self):
-		self.pid.reset()
+		self.throttle_pid.reset()
